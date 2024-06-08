c92b821eddf70b814d83e7e94daea728c48b153c
datehoer <datehoer@gmail.com>
2024-06-08 18:30:30 +0800

    对菜品进行操作后,删除缓存

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,7 +1,9 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
@@ -10,9 +12,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -21,6 +25,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     @PostMapping
     @ApiOperation("新增菜品")
     public Result save(@RequestBody DishDTO dishDTO){
@@ -34,6 +40,8 @@ public class DishController {
     public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
         log.info("查询菜品：{}", dishPageQueryDTO);
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        String key = "dish_" + dishPageQueryDTO.getCategoryId();
+        deleteRedisCache(key);
         return Result.success(pageResult);
     }
     @DeleteMapping
@@ -41,6 +49,8 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("删除菜品：{}", ids);
         dishService.deleteBatch(ids);
+
+        deleteRedisCache("dish_*");
         return Result.success();
     }
     @GetMapping("/{id}")
@@ -56,6 +66,7 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+        deleteRedisCache("dish_*");
         return Result.success();
     }
 
@@ -65,5 +76,22 @@ public class DishController {
         log.info("修改菜品状态：{}", id);
         dishService.updateStatus(id, status);
         return Result.success();
+    }
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<DishVO>> list(Long categoryId) {
+        Dish dish = new Dish();
+        dish.setCategoryId(categoryId);
+        dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
+
+        List<DishVO> dishVOList = dishService.listWithFlavor(dish);
+        return Result.success(dishVOList);
+    }
+
+    private void deleteRedisCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        if (keys != null && !keys.isEmpty()){
+            redisTemplate.delete(keys);
+        }
     }
 }
